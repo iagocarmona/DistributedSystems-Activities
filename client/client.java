34e200d0cdfa7e5218be24550dab26b2e2915bf2
Iago Carmona <iagoortegacarmona@gmail.com>
2023-08-30 21:36:59 -0300

    :rocket: Removendo docker

--- client/client.java
+++ client/client.java
@@ -1,35 +1,43 @@
-/**
- * TCPClient: Cliente para conexao TCP
- * Descrição: Envia uma informacao ao servidor e finaliza a conexao
- */
-
 import java.net.*;
 import java.io.*;
 
 public class client {
-	public static void main (String args[]) {
-            /* args[0]: mensagem  e args[1]: ip do servidor */
+    public static void main(String args[]) {
+        Socket s = null;
+        try {
+            int serverPort = 7896;   /* especifica a porta */
+            String ip = "localhost";
+            s = new Socket(ip, serverPort);  /* conecta com o servidor */  
+
+            /* cria objetos de leitura e escrita */
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            // Mantém a conexão ativa até enviar a mensagem "exit"
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String message;
             
-	    Socket s = null;
-            try{
-                int serverPort = 7896;   /* especifica a porta */
-                s = new Socket(args[1], serverPort);  /* conecta com o servidor */  
-                
-                /* cria objetos de leitura e escrita */
-                DataInputStream in = new DataInputStream( s.getInputStream());
-                DataOutputStream out =new DataOutputStream( s.getOutputStream());
-                
-                out.writeUTF(args[0]);      	// envia uma string para o servidor
-		
-		System.out.println ("Informação enviada.");
-		in.close(); out.close(); s.close();   //finaliza a conexao
-            
-	    } catch (UnknownHostException e){
-		System.out.println("Socket:"+e.getMessage());
-            } catch (EOFException e){
-		System.out.println("EOF:"+e.getMessage());
-            } catch (IOException e){
-		System.out.println("leitura:"+e.getMessage());
-            } //catch
-     } //main
-} //class
+            while (true) {
+                message = reader.readLine();
+                out.writeUTF(message);  // envia a mensagem para o servidor
+
+                if (message.equalsIgnoreCase("exit")) {
+                    break;  // Sai do loop se a mensagem for "exit"
+                }
+
+                String response = in.readUTF();  // aguarda resposta do servidor
+                System.out.println("Resposta do servidor: " + response);
+            }
+
+            in.close();
+            out.close();
+            s.close();  // finaliza a conexão
+        } catch (UnknownHostException e) {
+            System.out.println("Socket:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("leitura:" + e.getMessage());
+        }
+    }
+}
--- server/server.java
+++ server/server.java
@@ -1,69 +1,65 @@
-/**
- * TCPServer: Servidor para conexao TCP com Threads
- * Descricao: Recebe uma conexao, cria uma thread, recebe uma mensagem e finaliza a conexao
- */
-
 import java.net.*;
 import java.io.*;
 
 public class server {
     public static void main (String args[]) {
-        try{
+        try {
             int serverPort = 7896; // porta do servidor
-            /* cria um socket e mapeia a porta para aguardar conexão */
             ServerSocket listenSocket = new ServerSocket(serverPort);
             
-            while(true) {
-                System.out.println ("Servidor aguardando conexão ...");
-		
-		        /* aguarda conexões */
+            while (true) {
+                System.out.println("Servidor aguardando conexão ...");
                 Socket clientSocket = listenSocket.accept();
                 
-                System.out.println ("Cliente conectado ... Criando thread ...");
-                /* cria um thread para atender a conexão */
+                System.out.println("Cliente conectado ... Criando thread ...");
                 Connection c = new Connection(clientSocket);
-            } //while
+            }
             
-        } catch(IOException e) {
-	    System.out.println("Listen socket:"+e.getMessage());
-	} //catch
-    } //main
-} //class
+        } catch (IOException e) {
+            System.out.println("Listen socket: " + e.getMessage());
+        }
+    }
 
+    static class Connection extends Thread {
+        DataInputStream in;
+        DataOutputStream out;
+        Socket clientSocket;
 
+        public Connection(Socket aClientSocket) {
+            try {
+                clientSocket = aClientSocket;
+                in = new DataInputStream(clientSocket.getInputStream());
+                out = new DataOutputStream(clientSocket.getOutputStream());
+                this.start();  /* inicializa a thread */
+            } catch (IOException e) {
+                System.out.println("Connection: " + e.getMessage());
+            }
+        }
 
-/**
- * Classe Connection: Thread responsavel pela comunicacao
- * Descricao: Rebebe um socket, cria os objetos de leitura e escrita e aguarda msgs clientes 
- */
-class Connection extends Thread {
-    DataInputStream in;
-    DataOutputStream out;
-    Socket clientSocket;
-    
-    public Connection (Socket aClientSocket) {
-        try {
-            clientSocket = aClientSocket;
-            in = new DataInputStream( clientSocket.getInputStream());
-            out =new DataOutputStream( clientSocket.getOutputStream());
-            this.start();  /* inicializa a thread */
-        } catch(IOException e) {
-	    System.out.println("Connection:"+e.getMessage());
-	} //catch
-    } //construtor
-    
-    /* metodo executado ao iniciar a thread - start() */
-    public void run(){
-        try {			                 
-            String data = in.readUTF();   /* aguarda o envio de dados */	 
-            System.out.println ("Cliente disse: " + data); 
+        public void run() {
+            try {
+                while (true) {
+                    String data = in.readUTF();   /* aguarda o envio de dados */
+                    System.out.println("Cliente disse: " + data);
 
-	    in.close(); out.close(); clientSocket.close();  /* finaliza a conexao com o cliente */
-	    
-        } catch (EOFException e){
-	    System.out.println("EOF: "+e.getMessage());
-        } catch(IOException e) {
-	    System.out.println("leitura: "+e.getMessage());
-        } //catch
-    } //run
-} //class
+                    if (data.equalsIgnoreCase("EXIT")) {
+                        break; // Encerrar a conexão se o cliente enviar "EXIT"
+                    }
+
+                    // Envie uma resposta de volta para o cliente
+                    String response = "Recebido: " + data;
+                    out.writeUTF(response);
+                }
+
+                System.out.println("Cliente desconectado.");
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (EOFException e) {
+                System.out.println("EOF: " + e.getMessage());
+            } catch (IOException e) {
+                System.out.println("Erro de leitura: " + e.getMessage());
+            }
+        }
+    }
+}
